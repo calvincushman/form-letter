2c1d3cc364df7f7d4ead8deb51c6bc59c249641a
Calvin Cushman and Kristen McKee <kristen@>
2017-04-26 10:04:36 -0700

    add css styles, well, fix form

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(function() {
+  $("#form").submit(function(event) {
+    var name = $("input#name").val();
+
+  $(".name").text(name);
+
+  $("#letter").show();
+
+  event.preventDefault();
+
+  });
+});
